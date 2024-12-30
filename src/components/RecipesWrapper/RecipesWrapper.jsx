3a70e6f755f2a5ca6e659b68aafd746d419c82cb
AlbertB93 <bercik93@interia.eu>
2024-12-30 14:39:28 +0100

    Add testing db dishes and ingredients

--- src/components/RecipesWrapper/RecipesWrapper.jsx
+++ src/components/RecipesWrapper/RecipesWrapper.jsx
@@ -1,5 +1,19 @@
 import styles from "./RecipesWrapper.module.css";
+import { useGetData } from "./../../hooks/useGetData";
 
 export function RecipesWrapper() {
-  return <div className={styles.recipesWrapper}>Recipes Wrapper</div>;
+  const {
+    data: dishes,
+    setData: setDishes,
+    setError,
+  } = useGetData("/db/dishes.json");
+
+  /* użyć useEffect? */
+
+  return (
+    <div className={styles.recipesWrapper}>
+      Recipes Wrapper
+      {console.log("Obiekt:" + dishes[0])}
+    </div>
+  );
 }
