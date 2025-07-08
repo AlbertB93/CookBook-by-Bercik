d96dd7851047d8c8130d8561cfb0884b7bbdb18d
AlbertB93 <bercik93@interia.eu>
2025-07-08 12:44:32 +0200

    I use useContext to add meals to DailyMenu

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,5 +1,68 @@
+import { useState } from "react";
 import styles from "./MainContent.module.css";
+import { ListOfMealsStateContext } from "../../contexts/ListOfMealsStateContext.js";
+import { SummaryOfDayStateContext } from "../../contexts/SummaryOfDayStateContext.js";
+
+const initialSummaryOfDay = { kcal: 0, fats: 0, carbons: 0, proteins: 0 };
 
 export function MainContent({ children }) {
-  return <div className={styles.mainContent}>{children}</div>;
+  const [listOfMeals, setListOfMeals] = useState([]);
+  const [summaryOfDay, setSummaryOfDay] = useState(initialSummaryOfDay);
+  /* New Meal */
+  const [valuesOfNewMeal, setValuesOfNewMeal] = useState({
+    kcal: 0,
+    fats: 0,
+    carbons: 0,
+    proteins: 0,
+  });
+
+  function addToDailyMenu(id, title, imgUrl, kcal, fats, carbons, proteins) {
+    setSummaryOfDay((prevState) => {
+      prevState.kcal += kcal;
+      prevState.fats += fats;
+      prevState.carbons += carbons;
+      prevState.proteins += proteins;
+      return { ...prevState };
+    });
+
+    setListOfMeals((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: id,
+          title: title,
+          img: imgUrl,
+          kcal: kcal,
+          fats: fats,
+          carbons: carbons,
+          proteins: proteins,
+        },
+      ];
+    });
+    setValuesOfNewMeal({ kcal: 0, fats: 0, carbons: 0, proteins: 0 });
+  }
+
+  function deleteFromMenu(id, kcal, fats, carbons, proteins) {
+    setListOfMeals((prevState) => prevState.filter((meal) => meal.id !== id));
+
+    setSummaryOfDay((prevState) => {
+      prevState.kcal -= kcal;
+      prevState.fats -= fats;
+      prevState.carbons -= carbons;
+      prevState.proteins -= proteins;
+      return { ...prevState };
+    });
+  }
+
+  return (
+    <ListOfMealsStateContext.Provider
+      value={[listOfMeals, addToDailyMenu, deleteFromMenu]}
+    >
+      <SummaryOfDayStateContext.Provider
+        value={[summaryOfDay, setSummaryOfDay]}
+      >
+        <div className={styles.mainContent}>{children}</div>;
+      </SummaryOfDayStateContext.Provider>
+    </ListOfMealsStateContext.Provider>
+  );
 }
--- src/components/RecipeAllRecipes/RecipeAllRecipes.jsx
+++ src/components/RecipeAllRecipes/RecipeAllRecipes.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import { ButtonSmall } from "../ButtonSmall/ButtonSmall";
 import styles from "./RecipeAllRecipes.module.css";
+import { ListOfMealsStateContext } from "../../contexts/ListOfMealsStateContext.js";
+import { useContext } from "react";
 
 export function RecipeAllRecipes({
   id,
@@ -11,20 +13,64 @@ export function RecipeAllRecipes({
   carbons,
   proteins,
 }) {
+  const [listOfMeals, addToDailyMenu] = useContext(ListOfMealsStateContext);
+
+  /*   function addToDailyMenu(
+    newId,
+    newTitle,
+    newImgUrl,
+    newKcal,
+    newFats,
+    newCarbons,
+    newProteins
+  ) {
+    setSummaryOfDay((prevState) => {
+      prevState.kcal += newKcal;
+      prevState.fats += newFats;
+      prevState.carbons += newCarbons;
+      prevState.proteins += newProteins;
+      return { ...prevState };
+    });
+
+    setListOfMeals((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: newId,
+          title: newTitle,
+          img: newImgUrl,
+          kcal: newKcal,
+          fats: newFats,
+          carbons: newCarbons,
+          proteins: newProteins,
+        },
+      ];
+    });
+  }
+ */
+
   return (
-    <div className={styles.recipeAllRecipes}>
-      <h3>{title}</h3>
-      <img src={imgUrl} alt="FOTKA" className={styles.imgContainer} />
-      <p>Wartości odżywcze:</p>
-      <p className={styles.values}>
-        {kcal} kcal. T: {fats}g. W: {carbons}g. B: {proteins} g.
-      </p>
-      <div className={styles.buttons}>
-        <ButtonSmall>
-          <NavLink to={`/przepis/${id}`}>Pokaż przepis</NavLink>
-        </ButtonSmall>
-        <ButtonSmall>Dodaj</ButtonSmall>
+    <ListOfMealsStateContext.Provider value={[listOfMeals, addToDailyMenu]}>
+      <div className={styles.recipeAllRecipes}>
+        <h3>{title}</h3>
+        <img src={imgUrl} alt="FOTKA" className={styles.imgContainer} />
+        <p>Wartości odżywcze:</p>
+        <p className={styles.values}>
+          {kcal} kcal. T: {fats}g. W: {carbons}g. B: {proteins} g.
+        </p>
+        <div className={styles.buttons}>
+          <ButtonSmall>
+            <NavLink to={`/przepis/${id}`}>Pokaż przepis</NavLink>
+          </ButtonSmall>
+          <ButtonSmall
+            onClick={() =>
+              addToDailyMenu(id, title, imgUrl, kcal, fats, carbons, proteins)
+            }
+          >
+            Dodaj
+          </ButtonSmall>
+        </div>
       </div>
-    </div>
+    </ListOfMealsStateContext.Provider>
   );
 }
